4b12047cf162f56dfba0c7003b80e25b3c31469e
Aswin-S-T <aswinswebart15@gmail.com>
2023-05-13 00:39:58 +0530

    login and get all post api integrated

--- web/src/screens/LoginScreen.jsx
+++ web/src/screens/LoginScreen.jsx
@@ -1,6 +1,42 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { BACKEND_URL } from "../constants/api";
 
 function LoginScreen() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
+
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		let loginData = {
+			email,
+			password,
+		};
+		await axios.post(`${BACKEND_URL}/user/login`, loginData).then((res) => {
+			if (res && res.data.status == 200) {
+				Swal.fire({
+					title: "Success!",
+					text: "Successfully loggedIn",
+					icon: "success",
+					confirmButtonText: "Ok",
+				}).then(() => {
+					localStorage.setItem("user", JSON.stringify(res.data.data));
+					navigate("/");
+				});
+			} else if (res && res.data.status == 400) {
+				Swal.fire({
+					title: "Error!",
+					text: "Something went wrong.",
+					icon: "error",
+					confirmButtonText: "Ok",
+				});
+			}
+		});
+	};
 	return (
 		<div>
 			<div className="registrationScreen">
@@ -10,7 +46,11 @@ function LoginScreen() {
 						<div className="container">
 							<div className="formBx p-4">
 								<h3 className="text-center">Login Here</h3>
-								<form class="row g-3 needs-validation mt-4" novalidate>
+								<form
+									class="row g-3 needs-validation mt-4"
+									novalidate
+									onSubmit={handleLogin}
+								>
 									<div class="col-md-6">
 										<label for="validationCustom03" class="form-label">
 											Email
@@ -20,6 +60,7 @@ function LoginScreen() {
 											class="form-control"
 											id="validationCustom03"
 											required
+											onChange={(e) => setEmail(e.target.value)}
 										/>
 										<div class="invalid-feedback">
 											Please provide a valid Email.
@@ -35,6 +76,7 @@ function LoginScreen() {
 											class="form-control"
 											id="validationCustom03"
 											required
+											onChange={(e) => setPassword(e.target.value)}
 										/>
 									</div>
 									<div class="col-12">
